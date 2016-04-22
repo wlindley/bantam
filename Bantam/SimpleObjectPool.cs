be9bf92c834142f828e1154e70941d640c3eed18
Walker Wondra-Lindley <walker@walkerlindley.com>
2016-04-21 23:03:31 -0700

    Basic implementation of object pool.

--- Bantam/SimpleObjectPool.cs
+++ Bantam/SimpleObjectPool.cs
@@ -1,11 +1,44 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Bantam
 {
 	public class SimpleObjectPool
 	{
-		public SimpleObjectPool ()
+		private Dictionary<Type, Queue> instances = new Dictionary<Type, Queue>();
+
+		public T Allocate<T>(Action<T> initializer = null) where T : class, new()
 		{
+			EnsurePoolExists<T>();
+			var instance = GetInstance<T>();
+			if (null != initializer)
+				initializer(instance);
+			return instance;
+		}
+
+		public void Release<T>(T instance)
+		{
+			EnsurePoolExists<T>();
+			instances[typeof(T)].Enqueue(instance);
+		}
+
+		private void EnsurePoolExists<T>()
+		{
+			var type = typeof(T);
+			if (!instances.ContainsKey(type))
+				instances[type] = new Queue();
+		}
+
+		private T GetInstance<T>() where T : class, new()
+		{
+			T instance;
+			var type = typeof(T);
+			if (0 < instances[type].Count)
+				instance = instances[type].Dequeue() as T;
+			else
+				instance = new T();
+			return instance;
 		}
 	}
 }
--- BantamTest/SimpleObjectPoolTest.cs
+++ BantamTest/SimpleObjectPoolTest.cs
@@ -13,5 +13,44 @@ namespace Bantam.Test
 		{
 			testObj = new SimpleObjectPool();
 		}
+
+		[Test]
+		public void AllocateReturnsInstanceOfType()
+		{
+			var instance = testObj.Allocate<DummyType>();
+			Assert.NotNull(instance);
+		}
+
+		[Test]
+		public void AllocateUsesProvidedInitializer()
+		{
+			var instance = testObj.Allocate<DummyType>((obj) =>
+			{
+				obj.value = 5;
+			});
+			Assert.AreEqual(5, instance.value);
+		}
+
+		[Test]
+		public void AllocateReturnsDifferentObjectsOnSubsequentCalls()
+		{
+			var first = testObj.Allocate<DummyType>();
+			var second = testObj.Allocate<DummyType>();
+			Assert.AreNotSame(first, second);
+		}
+
+		[Test]
+		public void ReleaseAllowsObjectToBeUsedAgain()
+		{
+			var first = testObj.Allocate<DummyType>();
+			testObj.Release<DummyType>(first);
+			var second = testObj.Allocate<DummyType>();
+			Assert.AreSame(first, second);
+		}
+	}
+
+	public class DummyType
+	{
+		public int value;
 	}
 }
