d46b1f8741f60c65aeea1d01ae84e0d8c485c441
Walker Lindley <walker@walkerlindley.com>
2016-04-23 14:48:56 -0700

    Moved CommandTemplate to its own file.

--- Bantam/CommandChain.cs
+++ Bantam/CommandChain.cs
@@ -3,32 +3,6 @@ using System.Collections.Generic;
 
 namespace Bantam
 {
-	internal abstract class CommandTemplate
-	{
-		public Type commandType;
-
-		public abstract Command AllocateCommand(ObjectPool pool, Event ev);
-		public abstract void FreeCommand(ObjectPool pool, Command command);
-	}
-
-	internal class CommandTemplate<T, U> : CommandTemplate where T : class, Event where U : Command, new()
-	{
-		public Action<U, T> initializer;
-
-		public override Command AllocateCommand(ObjectPool pool, Event ev)
-		{
-			var command = pool.Allocate<U>();
-			if (null != initializer)
-				initializer(command as U, ev as T);
-			return command;
-		}
-
-		public override void FreeCommand(ObjectPool pool, Command command)
-		{
-			pool.Free<U>(command as U);
-		}
-	}
-
 	public abstract class CommandChain
 	{
 		internal List<CommandTemplate> Commands = new List<CommandTemplate>();
---
+++ Bantam/CommandTemplate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Bantam
+{
+	internal abstract class CommandTemplate
+	{
+		public Type commandType;
+
+		public abstract Command AllocateCommand(ObjectPool pool, Event ev);
+		public abstract void FreeCommand(ObjectPool pool, Command command);
+	}
+
+	internal class CommandTemplate<T, U> : CommandTemplate where T : class, Event where U : Command, new()
+	{
+		public Action<U, T> initializer;
+
+		public override Command AllocateCommand(ObjectPool pool, Event ev)
+		{
+			var command = pool.Allocate<U>();
+			if (null != initializer)
+				initializer(command as U, ev as T);
+			return command;
+		}
+
+		public override void FreeCommand(ObjectPool pool, Command command)
+		{
+			pool.Free<U>(command as U);
+		}
+	}
+}
