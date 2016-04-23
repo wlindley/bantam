e834704359e7e997be4b680f5cd9ebdea1427ffb
Walker Lindley <walker@walkerlindley.com>
2016-04-23 11:35:11 -0700

    Added Allocate and Release variants that take type parameters instead of using generics.

--- Bantam/ObjectPool.cs
+++ Bantam/ObjectPool.cs
@@ -6,7 +6,7 @@ namespace Bantam
 {
 	public class ObjectPool
 	{
-		private Dictionary<Type, Queue> instances = new Dictionary<Type, Queue>();
+		private Dictionary<Type, Queue<Poolable>> instances = new Dictionary<Type, Queue<Poolable>>();
 
 		public T Allocate<T>() where T : class, Poolable, new()
 		{
@@ -14,17 +14,51 @@ namespace Bantam
 			return GetInstance<T>();
 		}
 
-		public void Release<T>(T instance)
+		public Poolable Allocate(Type type)
 		{
+			ValidateType(type);
+			EnsurePoolExists(type);
+			return GetInstance(type);
+		}
+
+		public void Release<T>(T instance) where T : Poolable
+		{
+			if (null == instance)
+				throw new NullInstanceException();
 			EnsurePoolExists<T>();
 			instances[typeof(T)].Enqueue(instance);
 		}
 
+		public void Release(Type type, Poolable instance)
+		{
+			if (null == instance)
+				throw new NullInstanceException();
+			ValidateType(type);
+			if (!type.IsInstanceOfType(instance))
+				throw new MismatchedTypeException();
+			EnsurePoolExists(type);
+			instances[type].Enqueue(instance);
+		}
+
 		private void EnsurePoolExists<T>()
 		{
-			var type = typeof(T);
+			EnsurePoolExists(typeof(T));
+		}
+
+		private void EnsurePoolExists(Type type)
+		{
 			if (!instances.ContainsKey(type))
-				instances[type] = new Queue();
+				instances[type] = new Queue<Poolable>();
+		}
+
+		void ValidateType(Type type)
+		{
+			if (!type.IsClass)
+				throw new InvalidTypeException();
+			if (null == type.GetConstructor(Type.EmptyTypes))
+				throw new InvalidTypeException();
+			if (!typeof(Poolable).IsAssignableFrom(type))
+				throw new InvalidTypeException();
 		}
 
 		private T GetInstance<T>() where T : class, Poolable, new()
@@ -38,5 +72,21 @@ namespace Bantam
 			instance.Reset();
 			return instance;
 		}
+
+		private Poolable GetInstance(Type type)
+		{
+			Poolable instance;
+			if (0 < instances[type].Count)
+				instance = instances[type].Dequeue();
+			else
+				instance = Activator.CreateInstance(type) as Poolable;
+			instance.Reset();
+			return instance;
+		}
 	}
+
+	public class ObjectPoolException : Exception {}
+	public class InvalidTypeException : ObjectPoolException {}
+	public class NullInstanceException : ObjectPoolException {}
+	public class MismatchedTypeException : ObjectPoolException {}
 }
--- BantamTest/ObjectPoolTest.cs
+++ BantamTest/ObjectPoolTest.cs
@@ -44,6 +44,71 @@ namespace Bantam.Test
 			var second = testObj.Allocate<DummyType>();
 			Assert.AreSame(first, second);
 		}
+
+		[Test]
+		public void ReleaseThrowsExceptionIfInstanceIsNull()
+		{
+			Assert.Throws<NullInstanceException>(() => testObj.Release<DummyType>(null));
+		}
+
+		[Test]
+		public void AllocateWithTypeParameterReturnsInstanceOfType()
+		{
+			var instance = testObj.Allocate(typeof(DummyType)) as DummyType;
+			Assert.NotNull(instance);
+		}
+
+		[Test]
+		public void AllocateWithTypeResetsObjectItReturns()
+		{
+			var instance = testObj.Allocate(typeof(DummyType)) as DummyType;
+			Assert.AreEqual(0, instance.value);
+		}
+
+		[Test]
+		public void AllocateWithTypeReturnsDifferentObjectsOnSubsequentCalls()
+		{
+			var first = testObj.Allocate(typeof(DummyType)) as DummyType;
+			var second = testObj.Allocate(typeof(DummyType)) as DummyType;
+			Assert.AreNotSame(first, second);
+		}
+
+		[Test]
+		public void ReleaseWithTypeAllowsObjectToBeUsedAgain()
+		{
+			var first = testObj.Allocate(typeof(DummyType)) as DummyType;
+			testObj.Release(typeof(DummyType), first);
+			var second = testObj.Allocate(typeof(DummyType)) as DummyType;
+			Assert.AreSame(first, second);
+		}
+
+		[Test]
+		public void AllocateWithTypeThrowsExceptionForInvalidTypes()
+		{
+			Assert.Throws<InvalidTypeException>(() => testObj.Allocate(typeof(NonPoolableType)));
+			Assert.Throws<InvalidTypeException>(() => testObj.Allocate(typeof(PoolableStruct)));
+			Assert.Throws<InvalidTypeException>(() => testObj.Allocate(typeof(PoolableWithConstructor)));
+		}
+
+		[Test]
+		public void ReleaseWithTypeThrowsExceptionForInvalidTypes()
+		{
+			Assert.Throws<InvalidTypeException>(() => testObj.Release(typeof(NonPoolableType), new DummyType()));
+			Assert.Throws<InvalidTypeException>(() => testObj.Release(typeof(PoolableStruct), new PoolableStruct()));
+			Assert.Throws<InvalidTypeException>(() => testObj.Release(typeof(PoolableWithConstructor), new PoolableWithConstructor(5)));
+		}
+
+		[Test]
+		public void ReleaseWithTypeThrowsExceptionIfInstanceIsNull()
+		{
+			Assert.Throws<NullInstanceException>(() => testObj.Release(typeof(DummyType), null));
+		}
+
+		[Test]
+		public void ReleaseWithTypeThrowsExceptionIfInstanceDoesNotMatchGivenType()
+		{
+			Assert.Throws<MismatchedTypeException>(() => testObj.Release(typeof(DummyType), new DummyEvent()));
+		}
 	}
 
 	public class DummyType : Poolable
@@ -53,6 +118,31 @@ namespace Bantam.Test
 		public void Reset()
 		{
 			value = 0;
+		}
+	}
+
+	public class NonPoolableType
+	{
+	}
+
+	public struct PoolableStruct : Poolable
+	{
+		public void Reset()
+		{
+			
+		}
+	}
+
+	public class PoolableWithConstructor : Poolable
+	{
+		public PoolableWithConstructor(int value)
+		{
+			
+		}
+
+		public void Reset()
+		{
+			
 		}
 	}
 }
